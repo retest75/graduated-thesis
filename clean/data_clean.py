92d69a6856f570a335094f7bfecae8759c0ccffc
retest75 <fk0963@gmail.com>
2024-02-28 20:05:49 +0800

    Revise and complete Inquiry class
    Complete the Inquiry class which inheriting Clean and can classify the
    raw data into normal(0) or disease(1), respectively.
    So far, we process the raw data successfully, next we can start doing
    some image preprocess.

--- clean/data_clean.py
+++ clean/data_clean.py
@@ -4,12 +4,11 @@
 # (2) after choosing the best one, we revise these filename and move back to original folder
 # (3) delete these repetitive file i.e., xxxxxxxx_L(1).jpg
 # (4) split entire dataset into left and right eyes
-
-#################################################################
-#                                                               #
-#               正式使用時,記得把初始化函數中的方法解開            #
-#                                                               #
-#################################################################
+#  after spliting, we label all image
+# (1) list each of years filename for particular report where 1: disease and 0: normal
+# (2) list image filename in entire dataset for certain year
+# (3) compare and take intersection these two lists
+# (4) move intersection file to destination folder
 
 import os
 import shutil
@@ -38,20 +37,20 @@ class Clean():
         self.years = years
         self.months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
         
-        #print("===== Data clean =====")
-        #for year in self.years:
-        #    self.path = os.path.join(self.root, year)                    # year
-        #    self.duplicated = os.path.join(self.path, "duplicated") # year/duplicated
+        print("===== Data clean =====")
+        for year in self.years:
+            self.path = os.path.join(self.root, year)                    # year
+            self.duplicated = os.path.join(self.path, "duplicated") # year/duplicated
 
-        #    for month in self.months:
-        #        self.duplicate(month) # copy repetitive file into another folder
-        #        self.move_back(month) # rename repetitive file and move back to original folder
-        #        self.delete()         # rename repetitive file
-        #        self.left_right(self.path)     # separate into left and right eye
-        #        self.len = self.length(folder="Left") + self.length(folder="Right")
-        #    print(f"Year: {year} complete, size = {self.len:5d}")
-        #print("===== Clean complete =====")
-        #print()        
+            for month in self.months:
+                self.duplicate(month) # copy repetitive file into another folder
+                self.move_back(month) # rename repetitive file and move back to original folder
+                self.delete()         # rename repetitive file
+                self.left_right(self.path)     # separate into left and right eye
+                self.len = self.length(folder="Left") + self.length(folder="Right")
+            print(f"Year: {year} complete, size = {self.len:5d}")
+        print("===== Clean complete, start classify =====")
+        print()        
         
     def length(self, folder):
         """ calculate size of dataset """
@@ -127,110 +126,80 @@ class Clean():
                     shutil.move(src, dst)
                 else:
                     continue
+                   
 
 class Inquiry(Clean):
-    """ Inherit Clean to make the entire dataset become smaller and label it """
-    
+    """ Inherit Clean to make the entire dataset become smaller and label it
+
+    Attribute
+    ------------
+    self.side    : left or right eyes
+    self.reports : report name and  its label
+
+    """
+
     def __init__(self, root, years):
         super().__init__(root, years)
 
-        self.sides = ["Left", "Right"]
-        self.reports = {"fundus":0, "fundus_and_sleep":1}
+        self.sides = {"Left":"L", "Right":"R"}
+        self.reports = {"fundus_and_sleep":1, "fundus":0}
 
-        for search in self.years:
-            for report in self.reports.keys():
-                mode = self.reports[report]
-                report_list = self.list_report_file(search, report)
+        for report_name in self.reports.keys():
+            for side in self.sides.keys():
+                report = self.report_list(report_name, self.sides[side]) # return all filename in some report
 
                 for year in self.years:
-                    img_list = self.list_org(year)
+                    img = self.img_list(year, side) # return all img filename in some year and side
+                    intersec = self.take_intersec(report, img)
+                    self.move(year, self.reports[report_name], side, intersec)
+            print(f"Report: {report_name} complete, label = {self.reports[report_name]}")
+        print("===== Classify complete =====")
 
-                    for side in self.sides:
-                        intersection = self.take_intersec(report_list, img_list[side], side)
-                        self.move(year, intersection, mode, side)
-
-    def list_report_file(self, year, report):
-        """ search image filename in report """
+    def report_list(self, report_name, side):
+        """ list all filename in some report """
 
         filename_list = []
-        report = report + "_" + year + ".xls"        # report filename
-        path = os.path.join(self.root, year, report) # /year/fundus_xxxx.xls
+        
+        for year in self.years:
+            report = report_name + "_" + year + ".xls"   # report filename
+            path = os.path.join(self.root, year, report) # /year/fundus_xxxx.xls
+            df = pd.read_excel(path)
 
-        df = pd.read_excel(path)
-
-        for idx in list(df["眼底鏡影像去個資編號"]):
-            if np.isnan(idx):
-                continue
-            else:
-                name = str(int(idx)) + ".jpg" # xxxxxxxxx.jpg
-                filename_list.append(name)
-
+            for idx in list(df["眼底鏡影像去個資編號"]):
+                if np.isnan(idx):
+                    continue
+                else:
+                    name = str(int(idx)) + "_" + side + ".jpg" # xxxxxxxxx_L.jpg
+                    filename_list.append(name)
         return filename_list
     
-    def list_org(self, year):
-        """ list original dataset images in particular year """
-
-        filename_dict = dict()
-
-        for side in self.sides:
-            path = os.path.join(self.root, year, side) # /year/Left
-            filename_dict[side] = os.listdir(path)     # {"Left":[xxxxxxxxx_L.jpg, ...], "Right":[xxxxxxxxx_R.jpg, ...]}
+    def img_list(self, year, side):
+        """ list all filename in some year """
         
-        return filename_dict
+        path = os.path.join(self.root, year, side) # /year/Left
+        return os.listdir(path)
     
-    def take_intersec(self, report_list, img_list, side):
-        """ take intersection between report and list """
-
-        if side == "Left":
-            for (idx, name) in enumerate(report_list):
-                report_list[idx] = name[0:9] + "_L.jpg"
-        else:
-            for (idx, name) in enumerate(report_list):
-                report_list[idx] = name[0:9] + "_R.jpg"
+    def take_intersec(self, report_list, img_list):
+        """ take intersection between report and all dataset """
 
         return list(set(report_list) & set(img_list))
-        #return report_list
     
-    def move(self, year, filename_list, mode, side):
-        """ move file from original to destination 
-        Parameter
-        ------------
-        mode : str, normal or disease
-        side : str, Left or Right
+    def move(self, year, mode, side, filename_list):
+        """ move file from original to dataset """
 
-        """
+        classify = {0:"0_normal", 1:"1_disease"}
 
-        if mode == 0:
-            if side == "Left":
-                for filename in filename_list:
-                    src = os.path.join(self.root, year, side, filename)                  # /year/Left/xxxxxxxxx_L.jpg
-                    dst = os.path.join(self.root, "dataset", "0_normal", side, filename) # /dataset/0_normal/Left/xxxxxxxxx_L.jpg
-                    shutil.move(src, dst)
-            else:
-                for filename in filename_list:
-                    src = os.path.join(self.root, year, side, filename)                  # /year/Right/xxxxxxxxx_R.jpg
-                    dst = os.path.join(self.root, "dataset", "0_normal", side, filename) # /dataset/0_normal/Right/xxxxxxxxx_R.jpg
-                    shutil.move(src, dst)
+        for file in filename_list:
+            src = os.path.join(self.root, year, side, file) # /year/Left/xxxxxxxxx_X.jpg
+            dst = os.path.join(self.root, "dataset", classify[mode], side, file) # /dataset/0_normal/Left/xxxxxxxxx_X.jpg
+            shutil.move(src, dst)
 
-        if mode == 1:
-            if side == "Left":
-                for filename in filename_list:
-                    src = os.path.join(self.root, year, side, filename)                   # /year/Left/xxxxxxxxx_L.jpg
-                    dst = os.path.join(self.root, "dataset", "1_disease", side, filename) # /dataset/1_disease/Left/xxxxxxxxx_L.jpg
-                    shutil.move(src, dst)
-            else:
-                for filename in filename_list:
-                    src = os.path.join(self.root, year, side, filename)                   # /year/Right/xxxxxxxxx_R.jpg
-                    dst = os.path.join(self.root, "dataset", "1_disease", side, filename) # /dataset/1_disease/Right/xxxxxxxxx_R.jpg
-                    shutil.move(src, dst)
-                    
 
 if __name__ == "__main__":
     root = "C:\\graduated\\thesis\\data"
     years = ["2022", "2021", "2020", "2019", "2018"]
-    #years = ["2018"]
 
     #clean = Clean(root, years)
     inquiry = Inquiry(root, years)
-    print()
+
     
