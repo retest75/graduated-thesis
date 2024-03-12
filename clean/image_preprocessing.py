0c32e7c030fcb0a73241a9c65cfe75b62012ff8d
retest75 <fk0963@gmail.com>
2024-03-12 10:41:36 +0800

    modify image_preprocess.py and let it can be used by dataset

--- clean/image_preprocessing.py
+++ clean/image_preprocessing.py
@@ -1,46 +1,46 @@
+# a module of preprocessing for image
+# input a folder name which include images
+# then you can use read_img method to read it and crop method to crop it to square-sized
+# you can utilize some for loops in __init__ method to read and crop also
+
 import os
-import numpy as np
 import cv2
 
 class Image_preprocess():
     def __init__(self, path):
         self.path = path
 
-        print(f"file path: {self.path}")
-
-        for filename in os.listdir(self.path):
-            pth = os.path.join(self.path, filename) # file path
-            img = self.read_img(pth)
-            img = self.crop(img)
-
     def read_img(self, img_path):
         """ Read image and transform to RGB mode """
 
         img = cv2.imread(img_path)                 # [H, W, C]
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # BGR -> RGB
-        print(f"Original shape = [{img.shape[0]}, {img.shape[1]}, {img.shape[2]}]")
+        #print(f"Original shape = [{img.shape[0]}, {img.shape[1]}, {img.shape[2]}]")
         
         return img
 
     def crop(self, img):
-        """ Crop image to square """
+        """ Crop image to square where orig shape = [1536, 2304, 3] """
 
-        img = img[:, 384:1920, :]
-        print(f"Cropped shape = [{img.shape[0]}, {img.shape[1]}, {img.shape[2]}]")
+        #img = img[:, 384:1920, :]       # shape: [H W, C] = [1536, 1536, 3]
+        img  = img[36:1500, 420:1884, :] # shape: [H W, C] = [1464, 1464, 3]
+        #print(f"Cropped shape = [{img.shape[0]}, {img.shape[1]}, {img.shape[2]}]")
         return img
 
 
 
 if __name__ == "__main__":
-    root = "/home/chenze/graduated/thesis/dataset"
-    condition = ["1_disease"]
-    direction = ["Left", "Right"]
+    root = "/Users/ChenZE/graduated/thesis/dataset/0_normal/Left/137988984_L.jpg"
+    #condition = ["1_disease"]
+    #direction = ["Left", "Right"]
 
-    for c in condition:
-        for d in direction:
-            path = os.path.join(root, c, d)
-            preprocess = Image_preprocess(path)
-
+    #for c in condition:
+    #    for d in direction:
+    #        path = os.path.join(root, c, d)
+    #        preprocess = Image_preprocess(path)
+    preprocess = Image_preprocess(path=None)
+    img = preprocess.crop(preprocess.read_img(root))
+    print(img.shape)
 
 
 
