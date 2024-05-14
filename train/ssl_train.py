c01ce43d489be65d216a99c82aea23e006dfc2e5
retest75 <fk0963@gmail.com>
2024-05-15 00:55:05 +0800

    add Evaluation stage to fine-tune or linear probe

--- train/ssl_train.py
+++ train/ssl_train.py
@@ -68,6 +68,12 @@ class Training():
             param = self.model.state_dict()
             torch.save(param, dst)
     
+    def get_param(self):
+        """ get hyper-parameter of optimizer """
+        groups = self.optimizer.param_groups[0]
+        
+        return groups["initial_lr"], groups["momentum"], groups["weight_decay"]
+
     def save_log(self, file: str, model_name: str, phase: str, optim: str, epoch: int, Epoch: int):
         """ save experiment log in epoch
         Parameter
@@ -75,12 +81,14 @@ class Training():
         epoch : int, current epoch
         Epoch : int, entire training epoch
         """
+        lr, momentum, weight_decay = self.get_param()
         with open(file, mode="a") as f:
             if (epoch+1) == 1:
                 f.write("======== Basic configuration ========\n")
                 f.write(f"Model: {model_name}\n")
                 f.write(f"Phase: {phase}\n")
                 f.write(f"Optimizer: {optim}\n")
+                f.write(f"Learing rate = {lr}, Momentun = {momentum}, Weight decay = {weight_decay}\n")
                 f.write("=====================================\n")
                 f.write(f"Epoch: {epoch+1}/{Epoch} | Loss: {self.loss[epoch]:.6f}\n")
             else:
@@ -91,3 +99,101 @@ class Training():
         with open(file, mode="a") as f:
             for loss in self.loss:
                 f.write(f"{loss}, ")
+
+class Evaluation(Training):
+    def __init__(self, device, model, dataset, dataloader, criterion, optimizer, scheduler=None):
+        """ 
+        Inherit Training class and use to evaluation by fine-tune or linear probe.
+        If you want to switch phase between fine-tine and linear probe, don't forget initializing
+        model, optimizer, sheduler, loss, acc
+        """
+        super().__init__(device, model, dataset, dataloader, criterion, optimizer, scheduler)
+        #---------- inherit these attributes ----------#
+        
+        # self.device = device
+        # self.model = model
+        # self.dataset = dataset
+        # self.dataloader = dataloader
+        # self.criterion = criterion
+        # self.optimizer = optimizer
+        # self.scheduler = scheduler
+
+        # self.len = self.dataset.__len__() # length of  evaluation dataset
+        # self.loss = []                    # evaluation loss
+        # self.lr = []                      # evaluation learning rate(備用)
+        #----------------------------------------------#
+
+        self.acc = []                       # acc
+
+    def eval_fn(self, epoch):
+        """ use fine-tune or linear probe to supervised learning """
+        since = time.time()
+        total_loss = 0.0
+        total_len = 0
+        correct = 0
+
+        self.model.train()
+        for (img, lbl) in self.dataloader:
+            img = img.to(self.device)
+            lbl = lbl.to(self.device)
+            print(img.size(0))
+
+            # forward-propagation
+            output = self.model(img) # output size: [batch, classes], by default classes = 2
+
+            # compute loss
+            loss = self.criterion(output, lbl)
+
+            # backward-propagation
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            # compute prediction and correct
+            _, pred = torch.max(output, 1)
+            correct += torch.sum(pred == lbl.data)
+
+            # compute average loss in each batch
+            total_loss += loss.item() * img.size(0)
+            total_len += img.size(0)
+            cur_loss = total_loss / total_len
+
+            # print imformation in batch
+            print(f"Epoch: [{epoch}] [{total_len:4d}/{self.len} | Loss: {cur_loss:.6f}]")
+        
+        # compute loss, acc and time
+        epoch_loss = total_loss / total_len
+        epoch_acc = correct / self.len
+        epoch_time = int(time.time() - since)
+
+        self.loss.append(epoch_loss)
+        self.acc.append(epoch_acc)
+        
+        return epoch_loss, epoch_acc, epoch_time
+
+    def save_log(self, file: str, model_name: str, phase: str, optim: str, epoch: int, Epoch: int):
+        """ save experiment log in epoch
+        Parameter
+        ------------
+        phase: str, only for fine_tune or Linear_probe
+        epoch : int, current epoch
+        Epoch : int, entire training epoch
+        """
+        lr, momentum, weight_decay = self.get_param()
+        with open(file, mode="a") as f:
+            if (epoch+1) == 1:
+                f.write("======== Basic configuration ========\n")
+                f.write(f"Model: {model_name}\n")
+                f.write(f"Phase: {phase}\n")
+                f.write(f"Optimizer: {optim}\n")
+                f.write(f"Learing rate = {lr}, Momentun = {momentum}, Weight decay = {weight_decay}\n")
+                f.write("=====================================\n")
+                f.write(f"Epoch: {epoch+1}/{Epoch} | Loss: {self.loss[epoch]:.6f} | Acc: {self.acc[epoch]*100:.2f}%\n")
+            else:
+                f.write(f"Epoch: {epoch+1}/{Epoch} | Loss: {self.loss[epoch]:.6f} | Acc: {self.acc[epoch]*100:.2f}%\n")
+
+    def save_acc(self, file: str):
+        """ save entire acc """
+        with open(file, mode="a") as f:
+            for acc in self.acc:
+                f.write(f"{acc}, ")
